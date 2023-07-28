b99762fb68998c23129d11713ba01858b1ce85ce
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2023-07-28 04:56:53 +0200

    Add a state machine for additional feedback.

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -25,28 +25,30 @@
           <h2 class="now-playing__artists" v-text="getTrackArtists"></h2>
         </div>
       </div>
-      <div class="now-playing__rating">
+      <div
+        v-if="feedbackPrimary"
+        class="now-playing__rating">
         <div class="now-playing__question">How do you like the current music?</div>
         <div class="now-playing__ratings">
-          <div class="rating-option rating-dissatisfied" @click="sendFeedback(2)">
+          <div class="rating-option rating-dissatisfied" @click="sendPrimaryFeedback(2)">
             <div class="rating-option__icon">
               <span class="material-symbols-outlined">sentiment_dissatisfied</span>
             </div>
             <div class="rating-option__text">Meh!</div>
           </div>
-          <div class="rating-option rating-neutral" @click="sendFeedback(3)">
+          <div class="rating-option rating-neutral" @click="sendPrimaryFeedback(3)">
             <div class="rating-option__icon">
               <span class="material-symbols-outlined">sentiment_neutral</span>
             </div>
             <div class="rating-option__text">Neutral</div>
           </div>
-          <div class="rating-option rating-satisfied" @click="sendFeedback(4)">
+          <div class="rating-option rating-satisfied" @click="sendPrimaryFeedback(4)">
             <div class="rating-option__icon">
               <span class="material-symbols-outlined">sentiment_satisfied</span>
             </div>
             <div class="rating-option__text">Good</div>
           </div>
-          <div class="rating-option rating-very-satisfied" @click="sendFeedback(5)">
+          <div class="rating-option rating-very-satisfied" @click="sendPrimaryFeedback(5)">
             <div class="rating-option__icon">
               <span class="material-symbols-outlined">sentiment_very_satisfied</span>
             </div>
@@ -54,10 +56,36 @@
           </div>
         </div>
       </div>
-    </div>
-    <div v-else class="now-playing" :class="getNowPlayingClass()">
-      <div class="now-playing__container">
-        <h1 class="now-playing__idle-heading">No music is playing 😔</h1>
+      <div
+        v-if="!feedbackPrimary"
+        class="now-playing__rating">
+        <div class="now-playing__question">What could be better?</div>
+        <div class="now-playing__ratings">
+          <div class="rating-option rating-slower" @click="sendAdditionalFeedback('Slower')">
+            <div class="rating-option__icon">
+              <span class="material-symbols-outlined">keyboard_double_arrow_down</span>
+            </div>
+            <div class="rating-option__text">Slower Music (Too fast)</div>
+          </div>
+          <div class="rating-option rating-faster" @click="sendAdditionalFeedback('Faster')">
+            <div class="rating-option__icon">
+              <span class="material-symbols-outlined">keyboard_double_arrow_up</span>
+            </div>
+            <div class="rating-option__text">Faster Music (Too slow)</div>
+          </div>
+          <div class="rating-option rating-music" @click="sendAdditionalFeedback('Music')">
+            <div class="rating-option__icon">
+              <span class="material-symbols-outlined">rotate_right</span>
+            </div>
+            <div class="rating-option__text">Different Music Style</div>
+          </div>
+          <div class="rating-option rating-air" @click="sendAdditionalFeedback('Air')">
+            <div class="rating-option__icon">
+              <span class="material-symbols-outlined">air</span>
+            </div>
+            <div class="rating-option__text">Air Break Please!</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -82,6 +110,8 @@ export default {
       pollPlaying: '',
       playerResponse: {},
       playerData: this.getEmptyPlayer(),
+      feedbackDeferred: {},
+      feedbackPrimary: true,
       colourPalette: '',
       swatches: []
     }
@@ -109,10 +139,26 @@ export default {
     /**
      * Make the network request to Google Forms to record a selected feedback button.
      */
-    async sendFeedback(sentimentNumber) {
+    async sendFeedback(sentimentNumber, suggestion, songName) {
+      await fetch(`https://docs.google.com/forms/d/e/1FAIpQLSdllaBvx8fsxDINUnXaRtRnKjANfxCX--5RUl0ts7JVjhgOKQ/formResponse?submit=Submit&usp=pp_url&entry.1463587533=${sentimentNumber}&entry.545794514=${songName}&entry.1789954540=${suggestion}`)
+    },
+
+    async sendPrimaryFeedback(sentimentNumber) {
       /* TODO: Extract this into a separate endpoint property */
       let songName = this.getSongNameAndArtist()
-      await fetch(`https://docs.google.com/forms/d/e/1FAIpQLSdllaBvx8fsxDINUnXaRtRnKjANfxCX--5RUl0ts7JVjhgOKQ/formResponse?submit=Submit&usp=pp_url&entry.1463587533=${sentimentNumber}&entry.545794514=${songName}`)
+      this.feedbackDeferred = {
+        song: songName,
+        sentiment: sentimentNumber
+      };
+      this.feedbackPrimary = false
+    },
+
+    async sendAdditionalFeedback(suggestion) {
+      /* TODO: Extract this into a separate endpoint property */
+      let d = this.feedbackDeferred
+      this.feedbackDeferred = {}
+      this.feedbackPrimary = true
+      this.sendFeedback(d.sentiment, suggestion, d.song)
     },
 
     getSongNameAndArtist() {
